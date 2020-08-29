e196c212b40a0b7dfacbafe27d9dafe7b879b539
Nazmul Huda <mhnazmul48@gmail.com>
2020-08-29 10:38:52 +0600

    Practice Rechart LineChart Added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,59 @@
 import React from 'react';
 import './App.css';
-import DomPractice from './components/DomPractice';
-import StatePractice from './components/StatePractice';
-import ConditionalReturnIfElse from './components/ConditionalReturnIfElse';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import ConditionalReturnElementVarriable from './components/ConditionalReturnElementVarriable';
-import RefreshNow from './components/RefreshNow';
-import DomRenderHydrade from './components/DomRenderHydrade';
-
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
 
 function App() {
-  return (   
+  const data = [
+    {
+      name: 'Nazmul', highest: 4000, lawest: 2400, 
+    },
+    {
+      name: 'Sania', highest: 3000, lawest: 1398, 
+    },
+    {
+      name: 'Billal', highest: 2000, lawest: 9800, 
+    },
+    {
+      name: 'Rashedul', highest: 2780, lawest: 3908, 
+    },
+    {
+      name: 'Hayder', highest: 1890, lawest: 4800, 
+    },
+    {
+      name: 'Shariful', highest: 2390, lawest: 3800, 
+    },
+    {
+      name: 'Babu', highest: 3490, lawest: 4300, 
+    },
+  ];
+  
+  return (
           <div>
-              <h5 className='text-center'>Change DOM using findDOMNode</h5>
-              <DomPractice></DomPractice>
-              <hr></hr>
-              <h5 className='text-center'>Change Container Using DOM render() or hydrade()</h5>
-              <DomRenderHydrade></DomRenderHydrade>
-              <hr></hr>
-              <h5 className='text-center'>Automaitce Force Update this.forceUpdate()</h5>
-              <RefreshNow></RefreshNow>
-              <hr></hr>
-              <h5 className='text-center'>Conditional Return inside return block</h5>
-              <ConditionalReturnElementVarriable></ConditionalReturnElementVarriable>
-              <hr></hr>
-              <h5 className='text-center'>Conditional Return Using State value Check</h5>
-              <ConditionalReturnIfElse></ConditionalReturnIfElse>
-              <hr></hr>
-              <h5 className='text-center'>State value set and Update</h5>
-              <StatePractice></StatePractice>
+            <h1 className='text-center jumbotron'>Recharts Practice Project</h1>
+            <div className="row">
+                <div className="col-md-6 mx-auto mt-4">
+                <LineChart
+                          width={500}
+                          height={300}
+                          data={data}
+                          margin={{
+                            top: 5, right: 30, left: 20, bottom: 5,
+                          }}
+                        >
+                          <CartesianGrid strokeDasharray="3 3" />
+                          <XAxis dataKey="name" />
+                          <YAxis />
+                          <Tooltip />
+                          <Legend />
+                          <Line type="monotone" dataKey="lawest" stroke="#8884d8" activeDot={{ r: 10 }} />
+                          <Line type="monotone" dataKey="highest" stroke="#82ca9d" />
+                  </LineChart>
+                </div>
+            </div>
           </div>
-   )
+        )
 }
 
 export default App;
